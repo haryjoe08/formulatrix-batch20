756a29269e0bc350218740d59c569bd4d6ebb621
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-03 14:14:16 +0700

    [Week 1] FooBar Exercise

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        LogicExcercise1 logic1 = new();
+
+        logic1.Generate(15);
+
+    }
+}
+
+class LogicExcercise1
+{
+    public void Generate(int count)
+    {
+        for (int i = 1; i <= count; i++)
+        {
+            System.Console.Write($"{GetText(i)}, ");
+        }
+    }
+
+    private static string GetText(int value)
+    {
+        if (value % 3 == 0 && value % 5 == 0) return "foobar";
+        if (value % 3 == 0) return "foo";
+        if (value % 5 == 0) return "bar";
+        return value.ToString();
+    }
+}

29d2598dcece4f2a58b8a1da812bdb95a76eab28
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-04 08:39:12 +0700

    [Week 1] Queue Exercise

---
+++ Day2/Program.cs
@@ -0,0 +1,35 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        QueueProcessor queue = new();
+
+        queue.Enqueue("A");
+        queue.Enqueue("B");
+        queue.Process();
+        queue.Process();
+    }
+}
+
+class QueueProcessor
+{
+    Queue<string> queue = new();
+    public void Enqueue(string val)
+    {
+        queue.Enqueue(val);
+        System.Console.WriteLine($"Queued {val}");
+    }
+
+    public void Process()
+    {
+        if (queue.Count == 0)
+        {
+            System.Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        string value = queue.Dequeue();
+        System.Console.WriteLine($"Processed {value}");
+    }
+
+}

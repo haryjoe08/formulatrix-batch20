5468cdc9cf0c46fcf9ce475ea4eda5ca7ce99f33
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-05 09:53:48 +0700

    [Week 1] Stack Excercise

---
+++ Day3/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        StackProcessor stackProcessor = new();
+        stackProcessor.Type("foo");
+        stackProcessor.Type("bar");
+        stackProcessor.Undo();
+        stackProcessor.Undo();
+    }
+}
+
+class StackProcessor
+{
+    readonly Stack<string> _stack = new();
+
+    public void Type(string value)
+    {
+        _stack.Push(value);
+        System.Console.WriteLine($"Typed {value}");
+    }
+
+    public void Undo()
+    {
+        if (_stack.Count == 0)
+        {
+            System.Console.WriteLine("Stack is empty");
+            return;
+        }
+        string value = _stack.Pop();
+        System.Console.WriteLine($"Undid {value}");
+    }
+
+}

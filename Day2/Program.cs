047076ceca22fb957080e1342e72df2739468d63
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-05 10:04:40 +0700

    Add readonly modifier and rename field in Enqueue method

--- Day2/Program.cs
+++ Day2/Program.cs
@@ -13,22 +13,22 @@
 
 class QueueProcessor
 {
-    Queue<string> queue = new();
+    readonly Queue<string> _queue = new();
     public void Enqueue(string val)
     {
-        queue.Enqueue(val);
+        _queue.Enqueue(val);
         System.Console.WriteLine($"Queued {val}");
     }
 
     public void Process()
     {
-        if (queue.Count == 0)
+        if (_queue.Count == 0)
         {
             System.Console.WriteLine("Queue is empty");
             return;
         }
 
-        string value = queue.Dequeue();
+        string value = _queue.Dequeue();
         System.Console.WriteLine($"Processed {value}");
     }
 

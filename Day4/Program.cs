ff6b761fc1b4f08fb80e69e2e0030f270a4f7921
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-06 09:06:09 +0700

    [Week-1] Linked List Exercise

---
+++ Day4/Program.cs
@@ -0,0 +1,64 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Sequence sequence = new();
+
+        sequence.Append(5);
+        sequence.Append(10);
+        sequence.Print();
+    }
+}
+
+class Node
+{
+    public int Value { get; set; }
+    public Node? Next { get; set; }
+
+    public Node(int value)
+    {
+        Value = value;
+    }
+}
+
+class Sequence
+{
+    private Node? head;
+    private Node? tail;
+
+    public void Append(int value)
+    {
+        Node newNode = new(value);
+
+        if (head == null)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+        else
+        {
+            tail!.Next = newNode;
+            tail = newNode;
+
+        }
+        System.Console.WriteLine($"Appended {value}");
+    }
+
+    public void Print()
+    {
+        Node? current = head;
+
+        System.Console.Write("Sequence: ");
+        while (current != null)
+        {
+            System.Console.Write($"{current.Value}");
+
+            if (current.Next != null)
+            {
+
+                System.Console.Write(" -> ");
+            }
+            current = current.Next;
+        }
+    }
+}

6ddb22416cbaf30064294a0cc9b30b7c45cbf5ab
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-10 08:34:16 +0700

    [Week 2] FooBar Exercise

---
+++ Day6/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        FooBarJazz.Foo(21);
+        FooBarJazz.Foo(35);
+        FooBarJazz.Foo(105);
+    }
+}
+
+class FooBarJazz
+{
+    public static void Foo(int x)
+    {
+        string result = "";
+
+        if (x % 3 == 0)
+        {
+            result += "foo";
+        }
+
+        if (x % 5 == 0)
+        {
+            result += "bar";
+        }
+
+        if (x % 7 == 0)
+        {
+            result += "jazz";
+        }
+
+        // if x is does not matching any of 3 conditions above
+        if (result == "")
+        {
+            result = x.ToString();
+        }
+
+        Console.WriteLine(result);
+    }
+}

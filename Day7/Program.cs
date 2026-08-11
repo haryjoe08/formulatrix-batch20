ec54036a432307f705b9632ce66325af638f89b2
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-11 10:15:31 +0700

    [Week 2] Queue Exercise

---
+++ Day7/Program.cs
@@ -0,0 +1,45 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        QueueProcessor queue = new();
+
+        queue.Enqueue("A");
+        queue.Enqueue("B");
+        queue.EnqueueVip("C");
+        queue.Process();
+        queue.Process();
+        queue.Process();
+    }
+}
+
+class QueueProcessor
+{
+    readonly LinkedList<string> _queue = new();
+    public void Enqueue(string val)
+    {
+        _queue.AddLast(val);
+        System.Console.WriteLine($"Queued {val}");
+    }
+
+    public void EnqueueVip(string val)
+    {
+        _queue.AddFirst(val);
+        System.Console.WriteLine($"VIP Queued {val}");
+    }
+
+    public void Process()
+    {
+        if (_queue.Count == 0)
+        {
+            System.Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        // null possibility already checked above
+        string value = _queue.First!.Value;
+        _queue.RemoveFirst();
+        System.Console.WriteLine($"Processed {value}");
+    }
+
+}

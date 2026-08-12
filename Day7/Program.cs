25b392f7d9d4fc4a9b53c2c3011206cebe6669c5
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-12 08:29:40 +0700

    [Week 2] Stack Exercise

--- Day7/Program.cs
+++ Day7/Program.cs
@@ -7,39 +7,55 @@
         queue.Enqueue("A");
         queue.Enqueue("B");
         queue.EnqueueVip("C");
+
         queue.Process();
         queue.Process();
         queue.Process();
     }
 }
 
+
 class QueueProcessor
 {
-    readonly LinkedList<string> _queue = new();
+    readonly Queue<string> _queue = new();
+
     public void Enqueue(string val)
     {
-        _queue.AddLast(val);
-        System.Console.WriteLine($"Queued {val}");
+        _queue.Enqueue(val);
+        Console.WriteLine($"Queued {val}");
     }
+
 
     public void EnqueueVip(string val)
     {
-        _queue.AddFirst(val);
-        System.Console.WriteLine($"VIP Queued {val}");
+        Queue<string> temp = new();
+
+        temp.Enqueue(val);
+
+        while (_queue.Count > 0)
+        {
+            temp.Enqueue(_queue.Dequeue());
+        }
+
+        while (temp.Count > 0)
+        {
+            _queue.Enqueue(temp.Dequeue());
+        }
+
+        Console.WriteLine($"VIP Queued {val}");
     }
+
 
     public void Process()
     {
         if (_queue.Count == 0)
         {
-            System.Console.WriteLine("Queue is empty");
+            Console.WriteLine("Queue is empty");
             return;
         }
 
-        // null possibility already checked above
-        string value = _queue.First!.Value;
-        _queue.RemoveFirst();
-        System.Console.WriteLine($"Processed {value}");
-    }
+        string value = _queue.Dequeue();
 
+        Console.WriteLine($"Processed {value}");
+    }
 }
---
+++ Day8/Program.cs
@@ -0,0 +1,66 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        StackProcessor stackProcessor = new();
+
+        stackProcessor.Type("A");
+        stackProcessor.Type("B");
+        stackProcessor.Type("C");
+        stackProcessor.Type("D");
+        stackProcessor.Undo();
+    }
+}
+
+class StackProcessor
+{
+    readonly Stack<string> _stack = new();
+    private const int _max = 3;
+
+    public void Type(string value)
+    {
+        if (_stack.Count == _max)
+        {
+            // Temporary stack for storing the stack except the oldest one
+            Stack<string> temp = new();
+
+            //  storing the stack except the oldest one to temporary stack
+            while (_stack.Count > 1)
+            {
+                temp.Push(_stack.Pop());
+            }
+
+            // pop the bottom value on the stack, which is the oldest one
+            _stack.Pop();
+
+            // restoring the remaining stack back to the original _stack
+            while (temp.Count > 0)
+            {
+                _stack.Push(temp.Pop());
+            }
+
+            // push the value to the stack
+            _stack.Push(value);
+
+            Console.WriteLine($"Dropped bottom, Typed {value}");
+            return;
+        }
+
+        // if stack < 3, push as usual
+        _stack.Push(value);
+        Console.WriteLine($"Typed {value}");
+    }
+
+    public void Undo()
+    {
+        if (_stack.Count == 0)
+        {
+            Console.WriteLine("Stack is empty");
+            return;
+        }
+
+        string value = _stack.Pop();
+        Console.WriteLine($"Undid {value}");
+    }
+}

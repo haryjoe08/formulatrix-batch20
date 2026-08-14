d62c816fd31a32c94925563e44220780124b332a
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-14 10:22:26 +0700

    [Week 2] Circular Queue Exercise

---
+++ Day10/Program.cs
@@ -0,0 +1,60 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        CircularQueue circularQueue = new(3);
+
+        circularQueue.Log(1);
+        circularQueue.Log(2);
+        circularQueue.Log(3);
+        circularQueue.Log(4);
+        circularQueue.Read();
+
+    }
+}
+
+class CircularQueue
+{
+    private readonly int[] _buffer;
+    private int _front;
+    private int _rear;
+    private int _count;
+
+    public CircularQueue(int capacity)
+    {
+        _buffer = new int[capacity];
+    }
+
+    public void Log(int value)
+    {
+        if (_count == _buffer.Length)
+        {
+            _buffer[_rear] = value;
+            _rear = (_rear + 1) % _buffer.Length;
+            _front = (_front + 1) % _buffer.Length;
+
+            Console.WriteLine($"Overwritten oldest with {value}");
+            return;
+        }
+
+        _buffer[_rear] = value;
+        _rear = (_rear + 1) % _buffer.Length;
+        _count++;
+
+        Console.WriteLine($"Logged {value}");
+    }
+
+    public void Read()
+    {
+        if (_count == 0)
+        {
+            Console.WriteLine("Buffer is empty");
+            return;
+        }
+
+        int value = _buffer[_front];
+        _front = (_front + 1) % _buffer.Length;
+        _count--;
+        Console.WriteLine($"Read {value}");
+    }
+}

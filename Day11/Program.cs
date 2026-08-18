cecbed5748632c1a4f9da190efcece6af3d63aa8
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-18 08:16:59 +0700

    [Week 3] Foobar Exercise

---
+++ Day11/Program.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        FooBarJazz.Foo(36);
+    }
+}
+
+class FooBarJazz
+{
+    public static void Foo(int x)
+    {
+        string result = "";
+
+        if (x % 3 == 0)
+        {
+            result += "foo";
+        }
+
+        if (x % 4 == 0)
+        {
+            result += "baz";
+        }
+
+        if (x % 5 == 0)
+        {
+            result += "bar";
+        }
+
+        if (x % 7 == 0)
+        {
+            result += "jazz";
+        }
+
+        if (x % 9 == 0)
+        {
+            result += "huzz";
+        }
+
+        // if x is does not matching any divisor
+        if (result == "")
+        {
+            result = x.ToString();
+        }
+
+        Console.WriteLine(result);
+    }
+}

34e37d591572e6dde827a7917ccbb222a2552d30
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-24 08:46:58 +0700

    [Week 4] FooBar Exercise

---
+++ Day16/Program.cs
@@ -0,0 +1,55 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        FooBarJazz fooBarJazz = new();
+
+        fooBarJazz.AddRule(3, "foo");
+        fooBarJazz.AddRule(5, "bar");
+
+        Console.WriteLine(fooBarJazz.GenerateSequence(1, 15));
+        Console.WriteLine(fooBarJazz.Evaluate(18));
+    }
+}
+
+class FooBarJazz
+{
+    private readonly Dictionary<int, string> _rules = new();
+
+    public void AddRule(int divisor, string output)
+    {
+        _rules.Add(divisor, output);
+    }
+
+    public string Evaluate(int number)
+    {
+        List<string> outputs = new();
+
+        foreach (var (divisor, output) in _rules.OrderBy(rule => rule.Key))
+        {
+            if (number % divisor == 0)
+            {
+                outputs.Add(output);
+            }
+        }
+
+        if (outputs.Count == 0)
+        {
+            return number.ToString();
+        }
+
+        return string.Join(" ", outputs);
+    }
+
+    public string GenerateSequence(int start, int end)
+    {
+        List<string> sequence = new();
+
+        for (int number = start; number <= end; number++)
+        {
+            sequence.Add(Evaluate(number));
+        }
+
+        return string.Join(", ", sequence);
+    }
+}

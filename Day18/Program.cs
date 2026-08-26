5966f04a2c42c191def2af1541008f159312a11b
Hary Zulfa <haryzulfa08@gmail.com>
2026-08-26 19:40:24 +0700

    [Week 4] Stack Exercise

---
+++ Day18/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        History history = new();
+
+        history.AddValidationRule(word => !string.IsNullOrWhiteSpace(word));
+        history.AddValidationRule(word => word.Length <= 10);
+
+        System.Console.WriteLine(history.Type("hello"));
+        System.Console.WriteLine(history.Type(""));
+    }
+}
+
+
+class History
+{
+    private readonly List<Func<string, bool>> _rules = new();
+
+    public void AddValidationRule(Func<string, bool> rule)
+    {
+        _rules.Add(rule);
+    }
+
+    public string Type(string word)
+    {
+        foreach (Func<string, bool> rule in _rules)
+        {
+            var result = rule.Invoke(word);
+            if(result == false)
+            {
+                return "Rejected";
+            }
+        }
+        return $"Typed {word}";
+    }
+}
